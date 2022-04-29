3d09ba67f03f3327aedaa02dd3a1f8b565c0974a
fengshanshan <fengshanshn@icloud.com>
2022-04-29 20:01:38 +0800

    fix test case

--- src/upstream/sybil_list/tests.rs
+++ src/upstream/sybil_list/tests.rs
@@ -1,26 +1,12 @@
 mod tests {
-    //use crate::upstream::sybil_list;
     use crate::{error::Error, upstream::sybil_list::SybilList, upstream::Fetcher};
-    //const PROOF_SERVICE_URL: &str = "https://proof-service.nextnext.id"; // Staging
 
     #[tokio::test]
     async fn test_get_sybil_result() -> Result<(), Error> {
         let sy: SybilList = SybilList {};
-        let result = sy.fetch(Some(" ".to_string())).await;
-        let _result = match result {
-            Ok(res) => {
-                let mut c = 1;
-                for i in res.iter() {
-                    c = c+1;
-                    if c > 2 {
-                        break;
-                    }
-                    println!("{:?}", i);
-                }
-                //println!("{:?}", res.fetch(&self, ""));
-            },
-            Err(error) => println!("{}", error),
-        };
+        let result = sy.fetch(Some(" ".to_string())).await?;
+        //println!("{:?}", result.first());
+        assert_ne!(result.len(), 0);
         Ok(())
     }
 }
