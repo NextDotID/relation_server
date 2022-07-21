11197871de2f688f3ae31460d1236266e246d844
Nyk Ma <nykma@mask.io>
2022-07-21 12:54:39 +0800

    [!] graphql/identity: make `platform` as enum type

--- src/upstream/mod.rs
+++ src/upstream/mod.rs
@@ -124,38 +124,55 @@ impl std::fmt::Display for Target {
 /// All identity platform.
 /// TODO: move this definition into `graph/vertex/identity`, since it is not specific to upstream.
 #[derive(
-    Serialize, Deserialize, Debug, EnumString, Clone, Display, PartialEq, EnumIter, Default,
+    Serialize,
+    Deserialize,
+    Debug,
+    EnumString,
+    Clone,
+    Copy,
+    Display,
+    PartialEq,
+    Eq,
+    EnumIter,
+    Default,
+    async_graphql::Enum,
 )]
 pub enum Platform {
     /// Twitter
     #[strum(serialize = "twitter")]
     #[serde(rename = "twitter")]
+    #[graphql(name = "twitter")]
     #[default]
     Twitter,
 
     /// Ethereum wallet `0x[a-f0-9]{40}`
     #[strum(serialize = "ethereum", serialize = "eth")]
     #[serde(rename = "ethereum")]
+    #[graphql(name = "ethereum")]
     Ethereum,
 
     /// NextID
     #[strum(serialize = "nextid")]
     #[serde(rename = "nextid")]
+    #[graphql(name = "nextid")]
     NextID,
 
     /// Keybase
     #[strum(serialize = "keybase")]
     #[serde(rename = "keybase")]
+    #[graphql(name = "keybase")]
     Keybase,
 
     /// Github
     #[strum(serialize = "github")]
     #[serde(rename = "github")]
+    #[graphql(name = "github")]
     Github,
 
     /// Unknown
     #[strum(serialize = "unknown")]
     #[serde(rename = "unknown")]
+    #[graphql(name = "unknown")]
     Unknown,
 }
 
