3b083d6ba5bc6cc299029d8f30c86893a2a2cd04
ZhongFuze <zella@mask.io>
2022-09-22 14:39:03 +0800

    fix: query platform

--- src/upstream/mod.rs
+++ src/upstream/mod.rs
@@ -184,7 +184,7 @@ pub enum Platform {
     Reddit,
 
     /// Lens
-    #[strum(serialize = "lens", serialize = "Lens")]
+    #[strum(serialize = "Lens", serialize = "lens")]
     #[serde(rename = "lens")]
     #[graphql(name = "lens")]
     Lens,
