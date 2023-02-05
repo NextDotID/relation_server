5ddaca2f399a0c06396ac75c16f55c48c60d8dc4
ZhongFuze <zella@mask.io>
2023-02-05 17:24:29 +0800

    add `fetch_account_by_domain`

--- src/upstream/unstoppable/mod.rs
+++ src/upstream/unstoppable/mod.rs
@@ -15,6 +15,8 @@ use serde::{Deserialize, Serialize};
 use tracing::{debug, error, info, warn};
 use uuid::Uuid;
 
+use super::types::target;
+
 #[derive(Deserialize, Debug, Clone)]
 pub struct BadResponse {
     pub code: String,
@@ -86,6 +88,38 @@ pub struct Records {
     pub btc_address: Option<String>,
     #[serde(rename = "crypto.ETH.address")]
     pub eth_address: Option<String>,
+}
+
+pub struct Unstoppable {}
+#[async_trait]
+impl Fetcher for Unstoppable {
+    async fn fetch(target: &Target) -> Result<TargetProcessedList, Error> {
+        if !Self::can_fetch(target) {
+            return Ok(vec![]);
+        }
+
+        match target {
+            Target::Identity(platform, identity) => {
+                fetch_connections_by_platform_identity(platform, identity).await
+            }
+            Target::NFT(_, _, _, _) => todo!(),
+        }
+    }
+
+    fn can_fetch(target: &Target) -> bool {
+        target.in_platform_supported(vec![Platform::Unstoppable, Platform::Ethereum])
+    }
+}
+
+async fn fetch_connections_by_platform_identity(
+    platform: &Platform,
+    identity: &str,
+) -> Result<TargetProcessedList, Error> {
+    match *platform {
+        Platform::Ethereum => fetch_domains_by_account(platform, identity).await,
+        Platform::Unstoppable => fetch_account_by_domain(platform, identity).await,
+        _ => Ok(vec![]),
+    }
 }
 
 async fn fetch_domains_by_account(
@@ -263,5 +297,126 @@ async fn fetch_account_by_domain(
     _platform: &Platform,
     identity: &str,
 ) -> Result<TargetProcessedList, Error> {
-    Ok(vec![])
+    let client = make_client();
+    let uri: http::Uri = format!("{}/domains/{}", C.upstream.unstoppable_api.url, identity)
+        .parse()
+        .map_err(|_err: InvalidUri| Error::ParamError(format!("Uri format Error {}", _err)))?;
+    let req = hyper::Request::builder()
+        .method(Method::GET)
+        .uri(uri)
+        .header("Authorization", "Bearer tokenId")
+        .body(Body::empty())
+        .expect("request builder");
+    let mut resp = client.request(req).await?;
+    if !resp.status().is_success() {
+        error!("Unstoppable fetch error, statusCode: {}", resp.status());
+    }
+    let result = match parse_body::<DomainResponse>(&mut resp).await {
+        Ok(result) => result,
+        Err(_) => {
+            let err: BadResponse = parse_body(&mut resp).await?;
+            error!(
+                "Unstoppable fetch error, Code: {}, Message: {}",
+                err.code, err.message
+            );
+            return Ok(vec![]);
+        }
+    };
+
+    if result.meta.registry.is_none() || result.meta.owner.is_none() {
+        return Ok(vec![]);
+    }
+
+    let db = new_db_connection().await?;
+    let eth_identity: Identity = Identity {
+        uuid: Some(Uuid::new_v4()),
+        platform: Platform::Ethereum,
+        identity: result.meta.owner.clone().unwrap().to_lowercase(),
+        created_at: None,
+        display_name: None,
+        added_at: naive_now(),
+        avatar_url: None,
+        profile_url: None,
+        updated_at: naive_now(),
+    };
+
+    let unstoppable_identity: Identity = Identity {
+        uuid: Some(Uuid::new_v4()),
+        platform: Platform::Unstoppable,
+        identity: result.meta.domain.clone(),
+        created_at: None,
+        display_name: Some(result.meta.domain.clone()),
+        added_at: naive_now(),
+        avatar_url: None,
+        profile_url: None,
+        updated_at: naive_now(),
+    };
+
+    let hold: Hold = Hold {
+        uuid: Uuid::new_v4(),
+        source: DataSource::Unstoppable,
+        transaction: None,
+        id: result.meta.token_id.unwrap_or("".to_string()),
+        created_at: None,
+        updated_at: naive_now(),
+        fetcher: DataFetcher::RelationService,
+    };
+
+    let eth_record = eth_identity.create_or_update(&db).await?;
+    let unstoppable_record = unstoppable_identity.create_or_update(&db).await?;
+    hold.connect(&db, &eth_record, &unstoppable_record).await?;
+    let target = Target::Identity(
+        Platform::Ethereum,
+        result.meta.owner.clone().unwrap().to_lowercase(),
+    );
+    // reverse = true
+    if result.meta.reverse {
+        let reverse_uri: http::Uri = format!(
+            "{}/reverse/{}",
+            C.upstream.unstoppable_api.url,
+            result.meta.owner.clone().unwrap().to_lowercase()
+        )
+        .parse()
+        .map_err(|_err: InvalidUri| Error::ParamError(format!("Uri format Error {}", _err)))?;
+
+        let reverse_req = hyper::Request::builder()
+            .method(Method::GET)
+            .uri(reverse_uri)
+            .header("Authorization", "Bearer tokenId")
+            .body(Body::empty())
+            .expect("request builder");
+
+        let mut reverse_resp = client.request(reverse_req).await?;
+        if !reverse_resp.status().is_success() {
+            error!(
+                "Unstoppable reverse fetch error, statusCode: {}",
+                reverse_resp.status()
+            );
+            return Ok(vec![target]);
+        }
+        let _reverse = match parse_body::<ReverseResponse>(&mut reverse_resp).await {
+            Ok(r) => r,
+            Err(_) => {
+                let err: BadResponse = parse_body(&mut resp).await?;
+                error!(
+                    "Unstoppable reverse fetch error, Code: {}, Message: {}",
+                    err.code, err.message
+                );
+                return Ok(vec![target]);
+            }
+        };
+        let resolve: Resolve = Resolve {
+            uuid: Uuid::new_v4(),
+            source: DataSource::Unstoppable,
+            system: DomainNameSystem::Unstoppable,
+            name: _reverse.meta.domain,
+            fetcher: DataFetcher::RelationService,
+            updated_at: naive_now(),
+        };
+        resolve
+            .connect(&db, &unstoppable_record, &eth_record)
+            .await?;
+    }
+
+    Ok(vec![target])
 }
