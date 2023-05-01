f0585f036a1e97b3f9b4194ec1c3de97d0e2d88b
ZhongFuze <zella@mask.io>
2023-05-02 06:41:25 +0800

    Rewrite `IdentityRecord` on tigergraph

--- src/tigergraph/vertex/mod.rs
+++ src/tigergraph/vertex/mod.rs
@@ -1,1 +1,31 @@
 mod identity;
+use async_trait::async_trait;
+pub use identity::{Identity, IdentityRecord};
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+
+/// All `Vertex` records.
+#[async_trait]
+#[typetag::serde(tag = "v_type", content = "attributes")]
+pub trait Vertex {
+    fn primary_key(&self) -> String;
+
+    fn vertex_type(&self) -> String;
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct VertexRecord<T> {
+    /// The `v_type` field of `TigerGraph` is <vertex-type>
+    pub v_type: String,
+
+    /// The `v_id` field of `TigerGraph` is <vertex-id>
+    pub v_id: String,
+
+    /// The attributes of <edge>
+    pub attributes: T,
+}
+
+// Define a custom trait with a function that takes multiple input parameters.
+pub trait FromWithParams<T> {
+    fn from_with_params(v_type: String, v_id: String, attributes: T) -> Self;
+}
