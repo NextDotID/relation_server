2bb5f405de5b17182c4e204f734aff7cd9120056
ZhongFuze <zella@mask.io>
2024-05-27 20:42:11 +0800

    [!] #146 Refactor upstream `the_graph` & `ens` batch_fetch

--- src/upstream/the_graph/mod.rs
+++ src/upstream/the_graph/mod.rs
@@ -3,12 +3,16 @@ mod tests;
 
 use crate::config::C;
 use crate::error::Error;
-use crate::tigergraph::edge::{Hold, Resolve};
+use crate::tigergraph::edge::{
+    Hold, HyperEdge, Resolve, Wrapper, HOLD_CONTRACT, HOLD_IDENTITY, HYPER_EDGE, RESOLVE,
+    RESOLVE_CONTRACT,
+};
 use crate::tigergraph::upsert::create_contract_to_identity_resolve_record;
 use crate::tigergraph::upsert::create_identity_domain_resolve_record;
 use crate::tigergraph::upsert::create_identity_to_contract_hold_record;
 use crate::tigergraph::upsert::{create_ens_identity_ownership, create_ens_identity_resolve};
-use crate::tigergraph::vertex::{Contract, Identity};
+use crate::tigergraph::vertex::{Contract, IdentitiesGraph, Identity};
+use crate::tigergraph::{EdgeList, EdgeWrapperEnum};
 use crate::upstream::{
     Chain, ContractCategory, DataFetcher, DataSource, DomainNameSystem, Fetcher, Platform, Target,
     TargetProcessedList,
@@ -179,6 +183,14 @@ impl Fetcher for TheGraph {
         perform_fetch(target).await
     }
 
+    async fn batch_fetch(target: &Target) -> Result<(TargetProcessedList, EdgeList), Error> {
+        if !Self::can_fetch(target) {
+            return Ok((vec![], vec![]));
+        }
+
+        batch_perform_fetch(target).await
+    }
+
     fn can_fetch(target: &Target) -> bool {
         target.in_platform_supported(vec![Platform::Ethereum])
             || target.in_nft_supported(vec![ContractCategory::ENS], vec![Chain::Ethereum])
@@ -255,6 +267,171 @@ async fn fetch_domains(target: &Target) -> Result<Vec<Domain>, Error> {
         }
     }
     Ok(merged_domains)
+}
+
+async fn batch_perform_fetch(target: &Target) -> Result<(TargetProcessedList, EdgeList), Error> {
+    let merged_domains = fetch_domains(target).await?;
+    if merged_domains.is_empty() {
+        info!(?target, "TheGraph: No result");
+        return Ok((vec![], vec![]));
+    }
+
+    let platform = match target {
+        Target::Identity(_platform, _identity) => *_platform,
+        Target::NFT(_chain, _category, _contract_addr, _ens_name) => Platform::ENS,
+    };
+
+    let mut next_targets: TargetProcessedList = vec![];
+    let mut edges = EdgeList::new();
+    let hv = IdentitiesGraph::default();
+
+    for domain in merged_domains.into_iter() {
+        let creation_tx = domain
+            .events
+            .first()
+            .map(|event| event.transaction_id.clone());
+        let ens_created_at = parse_timestamp(&domain.created_at).ok();
+        let ens_expired_at = match &domain.registration {
+            Some(registration) => parse_timestamp(&registration.expiry_date).ok(),
+            None => None,
+        };
+
+        let owner = Identity {
+            uuid: Some(Uuid::new_v4()),
+            platform: Platform::Ethereum,
+            identity: domain.owner.id.clone(),
+            uid: None,
+            created_at: None,
+            display_name: None,
+            added_at: naive_now(),
+            avatar_url: None,
+            profile_url: None,
+            updated_at: naive_now(),
+            expired_at: None,
+            reverse: Some(false),
+        };
+        let ens_domain: Identity = Identity {
+            uuid: Some(Uuid::new_v4()),
+            platform: Platform::ENS,
+            identity: domain.name.clone(),
+            uid: None,
+            created_at: ens_created_at,
+            display_name: Some(domain.name.clone()),
+            added_at: naive_now(),
+            avatar_url: None,
+            profile_url: None,
+            updated_at: naive_now(),
+            expired_at: ens_expired_at,
+            reverse: Some(false),
+        };
+        let contract = Contract {
+            uuid: Uuid::new_v4(),
+            category: ContractCategory::ENS,
+            address: ContractCategory::ENS.default_contract_address().unwrap(),
+            chain: Chain::Ethereum,
+            symbol: None,
+            updated_at: naive_now(),
+        };
+        let ownership: Hold = Hold {
+            uuid: Uuid::new_v4(),
+            transaction: creation_tx,
+            id: domain.name.clone(),
+            source: DataSource::TheGraph,
+            created_at: ens_created_at,
+            updated_at: naive_now(),
+            fetcher: DataFetcher::RelationService,
+            expired_at: ens_expired_at,
+        };
+
+        edges.push(EdgeWrapperEnum::new_hyper_edge(
+            HyperEdge {}.wrapper(&hv, &owner, HYPER_EDGE),
+        ));
+
+        let resolved_address = domain.resolved_address.map(|r| r.id);
+        match resolved_address.clone() {
+            None => {
+                // Resolve record not existed anymore. Save owner address.
+                trace!(
+                    ?target,
+                    "TheGraph: Resolve address not existed. Save owner address"
+                );
+                // hold record
+                let hd = ownership.wrapper(&owner, &ens_domain, HOLD_IDENTITY);
+                let hdc = ownership.wrapper(&owner, &contract, HOLD_CONTRACT);
+                edges.push(EdgeWrapperEnum::new_hold_identity(hd));
+                edges.push(EdgeWrapperEnum::new_hold_contract(hdc));
+                // Append up_next
+                if platform == Platform::ENS {
+                    next_targets.push(Target::Identity(
+                        Platform::Ethereum,
+                        domain.owner.id.clone(),
+                    ));
+                }
+            }
+            Some(address) => {
+                // Filter zero address (without last 4 digits)
+                if !address.starts_with("0x000000000000000000000000000000000000") {
+                    // Create resolve record
+                    debug!(?target, address, domain = domain.name, "TheGraph: Resolved");
+                    let resolve = Resolve {
+                        uuid: Uuid::new_v4(),
+                        source: DataSource::TheGraph,
+                        system: DomainNameSystem::ENS,
+                        name: domain.name.clone(),
+                        fetcher: DataFetcher::RelationService,
+                        updated_at: naive_now(),
+                    };
+
+                    let domain_owner = domain.owner.id.clone();
+
+                    if address == domain_owner {
+                        // ens_domain will be added to hyper_vertex IdentitiesGraph
+                        // only when resolvedAddress == owner
+                        edges.push(EdgeWrapperEnum::new_hyper_edge(HyperEdge {}.wrapper(
+                            &hv,
+                            &ens_domain,
+                            HYPER_EDGE,
+                        )));
+
+                        // hold record
+                        let hd = ownership.wrapper(&owner, &ens_domain, HOLD_IDENTITY);
+                        let hdc = ownership.wrapper(&owner, &contract, HOLD_CONTRACT);
+                        edges.push(EdgeWrapperEnum::new_hold_identity(hd));
+                        edges.push(EdgeWrapperEnum::new_hold_contract(hdc));
+
+                        // resolve record
+                        let rs = resolve.wrapper(&ens_domain, &owner, RESOLVE);
+                        let rsc = resolve.wrapper(&contract, &owner, RESOLVE_CONTRACT);
+                        edges.push(EdgeWrapperEnum::new_resolve(rs));
+                        edges.push(EdgeWrapperEnum::new_resolve_contract(rsc));
+
+                        // Append up_next
+                        if platform == Platform::ENS {
+                            next_targets
+                                .push(Target::Identity(Platform::Ethereum, domain_owner.clone()));
+                        }
+                    } else {
+                        debug!(
+                            ?target,
+                            address, domain_owner, "TheGraph: address != domain_owner"
+                        );
+                        // hold record
+                        let hd = ownership.wrapper(&owner, &ens_domain, HOLD_IDENTITY);
+                        let hdc = ownership.wrapper(&owner, &contract, HOLD_CONTRACT);
+                        edges.push(EdgeWrapperEnum::new_hold_identity(hd));
+                        edges.push(EdgeWrapperEnum::new_hold_contract(hdc));
+                        // Append up_next
+                        if platform == Platform::ENS {
+                            next_targets
+                                .push(Target::Identity(Platform::Ethereum, domain_owner.clone()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    next_targets.dedup();
+    return Ok((next_targets, edges));
 }
 
 /// Focus on `ENS Domains` Saveing.
