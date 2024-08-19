d6f248d94f0b512ac4a1a66716b1e2303567236c
ZhongFuze <zella@mask.io>
2024-08-20 02:48:01 +0800

    [#] upgrade rss3 api

--- src/upstream/rss3/mod.rs
+++ src/upstream/rss3/mod.rs
@@ -4,31 +4,57 @@ mod tests;
 use crate::config::C;
 use crate::error::Error;
 use crate::tigergraph::edge::{Hold, HyperEdge, Wrapper, HOLD_CONTRACT, HYPER_EDGE};
-use crate::tigergraph::upsert::create_identity_to_contract_hold_record;
 use crate::tigergraph::vertex::{Contract, IdentitiesGraph, Identity};
 use crate::tigergraph::{EdgeList, EdgeWrapperEnum};
 use crate::upstream::{
     Chain, ContractCategory, DataSource, Fetcher, Platform, Target, TargetProcessedList,
 };
-use crate::util::{
-    make_client, make_http_client, naive_now, parse_body, request_with_timeout, utc_to_naive,
-};
+use crate::util::{make_client, naive_now, parse_body, request_with_timeout, timestamp_to_naive};
 use async_trait::async_trait;
-use futures::future::join_all;
 use http::uri::InvalidUri;
 use hyper::{Body, Method};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::str::FromStr;
-use tracing::{error, info};
+use tracing::{debug, error};
 use uuid::Uuid;
 
 use super::DataFetcher;
 
-#[derive(Deserialize, Debug, Clone)]
-pub struct Rss3Response {
-    pub total: i64,
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Rss3ErrorResponse {
+    pub error: String,
+    pub error_code: String,
+    pub detail: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct GetAccountActivitiesResponse {
+    pub data: Option<Vec<Activitity>>,
+    pub meta: Option<Cursor>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Cursor {
     pub cursor: Option<String>,
-    pub result: Vec<ResultItem>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Activitity {
+    pub id: String,
+    pub owner: String,
+    pub network: String,
+    pub index: i32,
+    #[serde(rename = "from")]
+    pub address_from: String,
+    #[serde(rename = "to")]
+    pub address_to: String,
+    pub tag: String,
+    #[serde(rename = "type")]
+    pub tag_type: String,
+    pub success: bool,
+    pub direction: String, // in/out/self
+    pub timestamp: i64,
+    pub actions: Vec<ActionItem>,
 }
 
 #[allow(dead_code)]
@@ -51,16 +77,14 @@ pub struct ResultItem {
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ActionItem {
     pub tag: String,
     #[serde(rename = "type")]
     pub tag_type: String,
-    #[serde(default)]
-    pub hash: String,
-    pub index: i64,
+    #[serde(rename = "from")]
     pub address_from: String,
-    #[serde(default)]
+    #[serde(rename = "to")]
     pub address_to: String,
     pub metadata: MetaData,
     #[serde(default)]
@@ -68,19 +92,21 @@ pub struct ActionItem {
 }
 
 #[allow(dead_code)]
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MetaData {
+    pub address: Option<String>,
     pub id: Option<String>,
-    pub name: Option<String>,
-    pub image: Option<String>,
     pub value: Option<String>,
+    pub name: Option<String>,
     pub symbol: Option<String>,
     pub standard: Option<String>,
-    pub contract_address: Option<String>,
-    pub handle: Option<String>,
+    pub decimals: Option<i32>,
+    pub image: Option<String>,            // deprecated
+    pub contract_address: Option<String>, // deprecated
+    pub handle: Option<String>,           // deprecated
 }
 
-const PAGE_LIMIT: i64 = 500;
+const PAGE_LIMIT: usize = 100;
 pub struct Rss3 {}
 
 #[async_trait]
@@ -91,7 +117,7 @@ impl Fetcher for Rss3 {
         }
 
         match target {
-            Target::Identity(platform, identity) => fetch_nfts_by_account(platform, identity).await,
+            Target::Identity(_platform, _identity) => todo!(),
             Target::NFT(_, _, _, _) => todo!(),
         }
     }
@@ -115,24 +141,24 @@ impl Fetcher for Rss3 {
 async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeList), Error> {
     let client = make_client();
     let address = target.identity()?.to_lowercase();
-    let mut cursor = String::from("");
+    let mut current_cursor = String::from("");
 
     let mut edges = EdgeList::new();
     let hv = IdentitiesGraph::default();
 
     loop {
         let uri: http::Uri;
-        if cursor.len() == 0 {
+        if current_cursor.len() == 0 {
             uri = format!(
-                "{}/{}?tag=collectible&include_poap=true&refresh=true",
+                "{}/decentralized/{}?tag=collectible&network=base,ethereum,optimism,polygon",
                 C.upstream.rss3_service.url, address
             )
             .parse()
             .map_err(|_err: InvalidUri| Error::ParamError(format!("Uri format Error {}", _err)))?;
         } else {
             uri = format!(
-                "{}/{}?tag=collectible&include_poap=true&refresh=true&cursor={}",
-                C.upstream.rss3_service.url, address, cursor
+                "{}/decentralized/{}?tag=collectible&network=base,ethereum,optimism,polygon&cursor={}",
+                C.upstream.rss3_service.url, address, current_cursor
             )
             .parse()
             .map_err(|_err: InvalidUri| Error::ParamError(format!("Uri format Error {}", _err)))?;
@@ -153,16 +179,51 @@ async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeL
                 ))
             })?;
 
-        let body: Rss3Response = parse_body(&mut resp).await?;
-        if body.total == 0 {
-            info!("Rss3 Response result is empty");
-            // break;
+        let body = match parse_body::<GetAccountActivitiesResponse>(&mut resp).await {
+            Ok(result) => result,
+            Err(_) => match parse_body::<Rss3ErrorResponse>(&mut resp).await {
+                Ok(rss3_info) => {
+                    let err_message = format!(
+                        "Rss3 Response error: {:?}, {:?}, {:?}",
+                        rss3_info.error_code, rss3_info.error, rss3_info.detail
+                    );
+                    error!(err_message);
+                    return Err(Error::ManualHttpClientError(err_message));
+                }
+                Err(err) => {
+                    let err_message = format!("Rss3 Response error parse_body error: {:?}", err);
+                    error!(err_message);
+                    return Err(Error::General(err_message, resp.status()));
+                }
+            },
+        };
+
+        if body.data.is_none() {
+            debug!("Rss3 Response result is empty");
+            break;
         }
 
-        let result: Vec<ResultItem> = body
-            .result
+        if let Some(meta) = body.meta {
+            match meta.cursor {
+                Some(cursor) => current_cursor = cursor,
+                None => current_cursor = String::from(""),
+            }
+        } else {
+            current_cursor = String::from("")
+        }
+
+        let result: Vec<Activitity> = body
+            .data
+            .clone()
+            .map_or(vec![], |data: Vec<Activitity>| data)
             .into_iter()
-            .filter(|p| p.owner == address)
+            .filter(|p| p.owner.to_lowercase() == address)
+            .filter(|p| {
+                p.network == "base"
+                    || p.network == "ethereum"
+                    || p.network == "optimism"
+                    || p.network == "polygon"
+            })
             .collect();
 
         for p in result.into_iter() {
@@ -184,8 +245,8 @@ async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeL
             if found.is_none() {
                 continue;
             }
-            let real_action = found.unwrap();
 
+            let real_action = found.unwrap();
             if real_action.metadata.symbol.is_none()
                 || real_action.metadata.symbol.as_ref().unwrap() == &String::from("ENS")
             {
@@ -204,14 +265,7 @@ async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeL
             if real_action.tag_type == "poap".to_string() {
                 nft_category = ContractCategory::POAP;
             }
-
-            let created_at_naive = match p.timestamp.as_ref() {
-                "" => None,
-                timestamp => match utc_to_naive(timestamp.to_string()) {
-                    Ok(naive_dt) => Some(naive_dt),
-                    Err(_) => None, // You may want to handle this error differently
-                },
-            };
+            let created_at_naive = timestamp_to_naive(p.timestamp, 0);
 
             let from: Identity = Identity {
                 uuid: Some(Uuid::new_v4()),
@@ -234,13 +288,20 @@ async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeL
                 error!("Rss3 Fetch data | Unknown Chain, original data: {:?}", p);
                 continue;
             }
+
             let contract_addr = real_action
                 .metadata
-                .contract_address
+                .address
                 .as_ref()
                 .unwrap()
                 .to_lowercase();
+
             let nft_id = real_action.metadata.id.as_ref().unwrap();
+            let tx = real_action
+                .related_urls
+                .first()
+                .cloned()
+                .unwrap_or("".to_string());
 
             let to: Contract = Contract {
                 uuid: Uuid::new_v4(),
@@ -254,7 +315,7 @@ async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeL
             let hold: Hold = Hold {
                 uuid: Uuid::new_v4(),
                 source: DataSource::Rss3,
-                transaction: Some(p.hash),
+                transaction: Some(tx),
                 id: nft_id.clone(),
                 created_at: created_at_naive,
                 updated_at: naive_now(),
@@ -268,201 +329,11 @@ async fn batch_fetch_nfts(target: &Target) -> Result<(TargetProcessedList, EdgeL
             let hdc = hold.wrapper(&from, &to, HOLD_CONTRACT);
             edges.push(EdgeWrapperEnum::new_hold_contract(hdc));
         }
-        if body.cursor.is_none() || body.total < PAGE_LIMIT {
+
+        if body.data.clone().unwrap().len() < PAGE_LIMIT {
             break;
-        } else {
-            cursor = body.cursor.unwrap();
         }
     }
+
     Ok((vec![], edges))
-}
-
-async fn fetch_nfts_by_account(
-    _platform: &Platform,
-    identity: &str,
-) -> Result<TargetProcessedList, Error> {
-    let mut cursor = String::from("");
-    let client = make_client();
-    let mut next_targets = Vec::new();
-
-    loop {
-        let uri: http::Uri;
-        if cursor.len() == 0 {
-            uri = format!(
-                "{}/{}?tag=collectible&include_poap=true&refresh=true",
-                C.upstream.rss3_service.url, identity
-            )
-            .parse()
-            .map_err(|_err: InvalidUri| Error::ParamError(format!("Uri format Error {}", _err)))?;
-        } else {
-            uri = format!(
-                "{}/{}?tag=collectible&include_poap=true&refresh=true&cursor={}",
-                C.upstream.rss3_service.url, identity, cursor
-            )
-            .parse()
-            .map_err(|_err: InvalidUri| Error::ParamError(format!("Uri format Error {}", _err)))?;
-        }
-
-        let req = hyper::Request::builder()
-            .method(Method::GET)
-            .uri(uri)
-            .body(Body::empty())
-            .map_err(|_err| Error::ParamError(format!("Rss3 Build Request Error {}", _err)))?;
-
-        let mut resp = request_with_timeout(&client, req, None)
-            .await
-            .map_err(|err| {
-                Error::ManualHttpClientError(format!(
-                    "Rss3 fetch fetch | error: {:?}",
-                    err.to_string()
-                ))
-            })?;
-
-        let body: Rss3Response = parse_body(&mut resp).await?;
-        if body.total == 0 {
-            info!("Rss3 Response result is empty");
-            break;
-        }
-
-        let futures: Vec<_> = body
-            .result
-            .into_iter()
-            .filter(|p| p.owner == identity.to_lowercase())
-            .map(save_item)
-            .collect();
-
-        let targets: TargetProcessedList = join_all(futures)
-            .await
-            .into_iter()
-            .flat_map(|result| result.unwrap_or_default())
-            .collect();
-
-        next_targets.extend(targets);
-        if body.cursor.is_none() || body.total < PAGE_LIMIT {
-            break;
-        } else {
-            cursor = body.cursor.unwrap();
-        }
-    }
-
-    Ok(next_targets)
-}
-
-async fn save_item(p: ResultItem) -> Result<TargetProcessedList, Error> {
-    // let creataed_at = DateTime::parse_from_rfc3339(&p.timestamp).unwrap();
-    // let created_at_naive = NaiveDateTime::from_timestamp_opt(creataed_at.timestamp(), 0);
-    let created_at_naive = match p.timestamp.as_ref() {
-        "" => None,
-        timestamp => match utc_to_naive(timestamp.to_string()) {
-            Ok(naive_dt) => Some(naive_dt),
-            Err(_) => None, // You may want to handle this error differently
-        },
-    };
-    let cli = make_http_client();
-
-    let from: Identity = Identity {
-        uuid: Some(Uuid::new_v4()),
-        platform: Platform::Ethereum,
-        identity: p.owner.to_lowercase(),
-        uid: None,
-        created_at: created_at_naive,
-        // Don't use ETH's wallet as display_name, use ENS reversed lookup instead.
-        display_name: None,
-        added_at: naive_now(),
-        avatar_url: None,
-        profile_url: None,
-        updated_at: naive_now(),
-        expired_at: None,
-        reverse: Some(false),
-    };
-
-    if p.actions.len() == 0 {
-        return Ok(vec![]);
-    }
-
-    let found = p
-        .actions
-        .iter()
-        // collectible (transfer, mint, burn) share the same UMS, but approve/revoke not.
-        // we need to record is the `hold` relation, so burn is excluded
-        .filter(|a| {
-            (a.tag_type == "transfer" && p.tag_type == "transfer")
-                || (a.tag_type == "mint" && p.tag_type == "mint")
-        })
-        .find(|a| (p.tag == "collectible" && a.tag == "collectible"));
-    if found.is_none() {
-        return Ok(vec![]);
-    }
-    let real_action = found.unwrap();
-
-    if real_action.metadata.symbol.is_none()
-        || real_action.metadata.symbol.as_ref().unwrap() == &String::from("ENS")
-    {
-        return Ok(vec![]);
-    }
-    let mut nft_category = ContractCategory::Unknown;
-    let standard = real_action.metadata.standard.clone();
-    if let Some(standard) = standard {
-        if standard == "ERC-721".to_string() {
-            nft_category = ContractCategory::ERC721;
-        } else if standard == "ERC-1155".to_string() {
-            nft_category = ContractCategory::ERC1155;
-        }
-    }
-
-    // let mut nft_category = ContractCategory::from_str(
-    //     real_action
-    //         .metadata
-    //         .standard
-    //         .as_ref()
-    //         .unwrap()
-    //         .to_lowercase()
-    //         .as_str(),
-    // )
-    // .unwrap_or_default();
-
-    if real_action.tag_type == "poap".to_string() {
-        nft_category = ContractCategory::POAP;
-    }
-
-    let chain = Chain::from_str(p.network.as_str()).unwrap_or_default();
-    if chain == Chain::Unknown {
-        error!("Rss3 Fetch data | Unknown Chain, original data: {:?}", p);
-        return Ok(vec![]);
-    }
-    let contract_addr = real_action
-        .metadata
-        .contract_address
-        .as_ref()
-        .unwrap()
-        .to_lowercase();
-    let nft_id = real_action.metadata.id.as_ref().unwrap();
-
-    let to: Contract = Contract {
-        uuid: Uuid::new_v4(),
-        category: nft_category,
-        address: contract_addr.clone(),
-        chain,
-        symbol: Some(real_action.metadata.symbol.as_ref().unwrap().clone()),
-        updated_at: naive_now(),
-    };
-
-    let hold: Hold = Hold {
-        uuid: Uuid::new_v4(),
-        source: DataSource::Rss3,
-        transaction: Some(p.hash),
-        id: nft_id.clone(),
-        created_at: created_at_naive,
-        updated_at: naive_now(),
-        fetcher: DataFetcher::RelationService,
-        expired_at: None,
-    };
-    create_identity_to_contract_hold_record(&cli, &from, &to, &hold).await?;
-
-    Ok(vec![Target::NFT(
-        chain,
-        nft_category,
-        contract_addr.clone(),
-        nft_id.clone(),
-    )])
 }
--- src/upstream/rss3/tests.rs
+++ src/upstream/rss3/tests.rs
@@ -1,42 +1,20 @@
-use std::collections::HashMap;
-
 use crate::{
     error::Error,
-    tigergraph::edge::Hold,
-    tigergraph::vertex::{Contract, Identity},
     upstream::rss3::Rss3,
-    upstream::{Chain, Platform},
+    upstream::Platform,
     upstream::{Fetcher, Target},
-    util::make_http_client,
 };
 
 #[tokio::test]
-async fn test_smoke_nft_rss3() -> Result<(), Error> {
+async fn test_fetch() -> Result<(), Error> {
     let target = Target::Identity(
         Platform::Ethereum,
-        "0x934b510d4c9103e6a87aef13b816fb080286d649".to_lowercase(),
+        "0xd8da6bf26964af9d7eed9e03e53415d37aa96045".to_lowercase(),
     );
-    let _ = Rss3::fetch(&target).await?;
-    let client = make_http_client();
+    let (_targets, all_edges) = Rss3::batch_fetch(&target).await?;
+    let json_raw_2 = serde_json::to_string(&all_edges).map_err(|err| Error::JSONParseError(err))?;
+    println!("all_edges: {}", json_raw_2);
+    println!("all_edges: {}", all_edges.len());
 
-    let owner =
-        Identity::find_by_platform_identity(&client, &Platform::Ethereum, &target.identity()?)
-            .await?
-            .expect("Record not found");
-    let contract = Contract::find_by_chain_address(
-        &client,
-        &Chain::Ethereum,
-        "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85",
-    )
-    .await?
-    .unwrap();
-
-    let filters = HashMap::from([("id".to_string(), "maskbook.eth".to_string())]);
-    let record = Hold::find_by_from_to(&client, &owner, &contract, Some(filters))
-        .await?
-        .and_then(|r| r.first().cloned())
-        .expect("Record not found");
-    let json_raw = serde_json::to_string(&record).map_err(|err| Error::JSONParseError(err))?;
-    println!("found: {}", json_raw);
     Ok(())
 }
